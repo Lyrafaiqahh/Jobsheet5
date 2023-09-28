c2ea4c8b5f460a89dcd72f58186689ccacbf340e
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-09-28 09:22:43 +0700

    INI PERCOBAAN 5 JAWABAN PERCOBAAN 1

--- PemilihanPercobaan115.java
+++ PemilihanPercobaan115.java
@@ -3,7 +3,7 @@ public class PemilihanPercobaan115 {
 
     public static void main(String[] args) {
        Scanner input15 = new Scanner (System.in);
-        System.out.println("Masukkan angka:  ");
+       System.out.println("Masukkan angka:  ");
        int angka = input15.nextInt();
        String hasil;
        hasil = (angka <=-1) ? " Bilangan negatif " : "Bilangan positif";

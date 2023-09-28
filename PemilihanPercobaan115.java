349cd9212dec9106db1b9e1e6b8971d02ef60f44
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-09-28 08:41:48 +0700

    INI JOBSHEET 5

---
+++ PemilihanPercobaan115.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PemilihanPercobaan115 {
+
+    public static void main(String[] args) {
+       Scanner input15 = new Scanner (System.in);
+        System.out.println("Masukkan angka:  ");
+       int angka = input15.nextInt();
+       String hasil;
+       hasil = (angka <=-1) ? " Bilangan negatif " : "Bilangan positif";
+       System.out.println(angka + " adalah " + hasil);
+    }
+}   
